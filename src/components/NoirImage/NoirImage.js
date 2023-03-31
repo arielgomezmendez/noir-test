f036f12b35bf9912de653ab506e45fb3a0a711e5
Ariel <arielgomezmendez94@gmail.com>
2023-03-31 15:14:01 -0400

    Readjust the size and the position of LoginForm and NoirImage components

---
+++ src/components/NoirImage/NoirImage.js
@@ -0,0 +1,12 @@
+import React from "react";
+import styles from "./styles.module.css";
+
+const NoirImage = () => {
+  return (
+    <>
+      <img src="/Bienvenido.png" className={styles.noirImage} />
+    </>
+  );
+};
+
+export default NoirImage;
--- src/pages/login/index.js
+++ src/pages/login/index.js
@@ -3,11 +3,12 @@ import NoirImage from "@/components/NoirImage/NoirImage";
 import { Container } from "@mui/system";
 //import { useRouter } from "next/router";
 import React from "react";
+import styles from "./styles.module.css";
 
 const index = () => {
   return (
     <>
-      <Container className="loginPage">
+      <Container className={styles.loginPage}>
         <NoirImage />
         <LoginForm />
       </Container>
