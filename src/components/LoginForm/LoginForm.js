d8c42f7d94c3c100c913f25b8bcbc46e2d91cecf
Ariel <arielgomezmendez94@gmail.com>
2023-03-31 08:06:30 -0400

    Create a Login.module.css for give styles to LoginForm component

--- src/components/LoginForm/LoginForm.js
+++ src/components/LoginForm/LoginForm.js
@@ -1,11 +1,13 @@
 import { Button, TextField } from "@mui/material";
 import { Container } from "@mui/system";
 import React from "react";
+//Import CSS how CSS componet level, to apply the styles just to LoginForm component using CSS modules
+import styles from "./LoginForm.module.css";
 
 const LoginForm = () => {
   return (
     <>
-      <Container>
+      <Container className={styles.loginContainer}>
         <h2>Bienvenido a Noir</h2>
         <span>Acceda a su cuenta</span>
 
