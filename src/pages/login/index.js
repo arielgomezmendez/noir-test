5570da0b25fe4b0ce05a87509f77ec15efb30262
Ariel <arielgomezmendez94@gmail.com>
2023-04-01 18:15:44 -0400

    Readjust the background color of the application and add the stars pictures

--- src/pages/login/index.js
+++ src/pages/login/index.js
@@ -1,18 +1,20 @@
 import LoginForm from "@/components/LoginForm/LoginForm";
 import NoirImage from "@/components/NoirImage/NoirImage";
 import { Container } from "@mui/system";
-//import { useRouter } from "next/router";
+import Image from "next/image";
 import React from "react";
 import styles from "./styles.module.css";
 
 const index = () => {
   return (
-    <>
+    <div className={styles.loginPageContainer}>
       <Container className={styles.loginPage}>
+        <Image src="/Looper2.png" alt="Looper 2" width={200} height={200} />
         <NoirImage />
         <LoginForm />
+        <Image src="/Looper-2-1.png" alt="Looper 2" width={200} height={200} />
       </Container>
-    </>
+    </div>
   );
 };
 
