f22de727a1788623df4474b1c5b40f5b51edc8e6
Ariel <arielgomezmendez94@gmail.com>
2023-04-07 15:21:02 -0400

    Install the nunito font from google fonts

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,9 +1,11 @@
 import "@/styles/globals.css";
 import { CssBaseline } from "@mui/material";
+import { GoogleFonts } from "next-google-fonts";
 
 export default function App({ Component, pageProps }) {
   return (
     <>
+      <GoogleFonts />
       <CssBaseline />
       <Component {...pageProps} />;
     </>
