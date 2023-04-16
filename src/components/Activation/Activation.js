108b8fbea38b4421fb368b1d60397cb4e2fad8dd
Ariel <arielgomezmendez94@gmail.com>
2023-04-16 11:33:08 -0400

    Add the styles file to the Activation componet
    Add the content to the Activation component

--- src/components/Activation/Activation.js
+++ src/components/Activation/Activation.js
@@ -1,10 +1,23 @@
 import React from "react";
+import styles from "./Activation.module.css";
+import { Button, Container, TextField } from "@mui/material";
 
 const Activation = () => {
   return (
-    <div>
-      <h1>Activacion</h1>
-    </div>
+    <>
+      <Container>
+        <h1>Activacion</h1>
+        <Container>
+          <TextField />
+          <TextField />
+          <TextField />
+          <TextField />
+        </Container>
+        <p>Hemos enviado un código de activación a:</p>
+        <p>correo@correo.com</p>
+        <Button variant="contained">Verificar</Button>
+      </Container>
+    </>
   );
 };
 
