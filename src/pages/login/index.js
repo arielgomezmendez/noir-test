622edda316229d3c1c52684d2a5cf8f661649d8d
Ariel <arielgomezmendez94@gmail.com>
2023-04-18 17:59:03 -0400

    Change the size of login page and reposition

--- src/pages/login/index.js
+++ src/pages/login/index.js
@@ -8,38 +8,36 @@ import styles from "./styles.module.css";
 const index = () => {
   return (
     <div className={styles.loginPageContainer}>
-      <Container className={styles.loginPage}>
-        {/*Left  side of the page*/}
-        <Container className={styles.leftSide}>
-          <Image
-            className={styles.star1}
-            src="/Looper-2-1.png"
-            alt="Looper 1"
-            width={500}
-            height={500}
-          />
-          <h1>Bienvenido a </h1>
-          <Image
-            src="/Notir.png"
-            width={180}
-            height={180}
-            alt="Notir"
-            className={styles.notirLogo}
-          />
-          <NotirImage className={styles.notirImageLogin} />
-        </Container>
+      {/*Left  side of the page*/}
+      <Container className={styles.leftSide}>
+        <Image
+          className={styles.star1}
+          src="/Looper-2-1.png"
+          alt="Looper 1"
+          width={500}
+          height={500}
+        />
+        <h1>Bienvenido a </h1>
+        <Image
+          src="/Notir.png"
+          width={180}
+          height={180}
+          alt="Notir"
+          className={styles.notirLogo}
+        />
+        <NotirImage className={styles.notirImageLogin} />
+      </Container>
 
-        {/*Riht  side of the page*/}
-        <Container className={styles.rightSide}>
-          <LoginForm />
-          <Image
-            className={styles.star2}
-            src="/Looper2.png"
-            alt="Looper 1"
-            width={500}
-            height={500}
-          />
-        </Container>
+      {/*Riht  side of the page*/}
+      <Container className={styles.rightSide}>
+        <LoginForm />
+        <Image
+          className={styles.star2}
+          src="/Looper2.png"
+          alt="Looper 1"
+          width={500}
+          height={500}
+        />
       </Container>
     </div>
   );
