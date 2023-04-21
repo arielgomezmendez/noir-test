765287d78782fa04abc3393ccd843224b2f1e5b0
Ariel <arielgomezmendez94@gmail.com>
2023-04-21 14:14:01 -0400

    Commit

--- src/pages/login/index.js
+++ src/pages/login/index.js
@@ -4,40 +4,27 @@ import { Container } from "@mui/system";
 import Image from "next/image";
 import React from "react";
 import styles from "./styles.module.css";
+import star1 from "../../../public/Looper-2-1.png";
+import star2 from "../../../public/Looper2.png";
+import notirLogo from "../../../public/Notir.png";
 
 const index = () => {
   return (
     <div className={styles.loginPageContainer}>
       {/*Left  side of the page*/}
       <Container className={styles.leftSide}>
-        <Image
-          className={styles.star1}
-          src="/Looper-2-1.png"
-          alt="Looper 1"
-          width={500}
-          height={500}
-        />
+        <Image className={styles.star1} src={star1} alt="Looper 1" />
+
         <h1>Bienvenido a </h1>
-        <Image
-          src="/Notir.png"
-          width={180}
-          height={180}
-          alt="Notir"
-          className={styles.notirLogo}
-        />
+
+        <Image src={notirLogo} alt="Notir" className={styles.notirLogo} />
         <NotirImage className={styles.notirImageLogin} />
       </Container>
 
       {/*Riht  side of the page*/}
       <Container className={styles.rightSide}>
         <LoginForm />
-        <Image
-          className={styles.star2}
-          src="/Looper2.png"
-          alt="Looper 1"
-          width={500}
-          height={500}
-        />
+        <Image className={styles.star2} src={star2} alt="Looper 2" />
       </Container>
     </div>
   );
